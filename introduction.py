235024a36de3b8f3d3eb58f8889916bd241e9040
dpdahal <dp48dahal@gmail.com>
2020-12-21 06:25:30 +0545

    upload githup project

---
+++ introduction.py
@@ -0,0 +1,75 @@
+# print("Hello", "Welcome", "Python")
+# print('mother\'s')
+# print('mother"s')
+# print("mother's")
+# print(1+5)
+"""
+Here is a list of the Python keywords.  Enter any keyword to get more help.
+False               break               for                 not
+None                class               from                or
+True                continue            global              pass
+__peg_parser__      def                 if                  raise
+and                 del                 import              return
+as                  elif                in                  try
+assert              else                is                  while
+async               except              lambda              with
+await               finally             nonlocal            yield
+"""
+# Name = "Sophia"
+# print(Name)
+# &*/<>
+# letter = ''
+# _ = ''
+
+# Camel Case = userName, nameOfStudent
+# Pascal Case = UserName, NameOfStudent
+# Snake Case = user_name, name_of_student
+# variable name = value
+# userName = "Sneha"
+# print("My name is: " + userName)
+
+# name = "Sneha"
+# address = "Kathmandu"
+# print("My name is: " + name + " Address:" + address)
+
+# name = "Sophia"
+# phone = 876787
+# print("Name: ", name, " Phone", phone)
+# Python old
+# print("Name: {} Phone {}".format(name, phone))
+# Python New
+# print(f"Name:{name} Phone: {phone}")
+
+# Max len = 79
+
+# userName = 1234
+# data = "Ram"
+
+# data = "Mother's"
+# data = "Sophia"
+# print(type(data))
+# print(id(data))
+# print(dir(data))
+
+# Number - integer,float,complex
+# Sequence - String,List,Tuple
+# Boolean - True or False,Null
+# Dictionary
+# Set
+
+# x = 1234567654323456765432112345678
+# print(x)
+# a = 45.67
+# print(type(a))
+print("Enter number:")
+x = input()
+x = int(x)
+
+print("Enter number:")
+y = input()
+y = int(y)
+
+print(x + y)
+
+a = '2'  # str
+a = int(a)  # int
---
+++ operators.py
@@ -0,0 +1,63 @@
+# ============Arithmetic = +,-, *, /, %, **, // ===========
+# a = 10
+# b = 20
+# print(a+b)
+# print(a-b)
+# print(type(a))
+# print(a % 2)
+# print(2 ** 5)  # 2*2*2*2*2
+# name = "abc"
+# print(f"Number:{a} Name:{name}")
+# print(a//3)
+
+# ======== Assignment Operator=== =,+=,-=,*=,/=,**=,//=,&=,!=,^=,>>=,<<=
+# a = 3
+# a += 4  # a=a+4
+
+# =========== Comparison ========= ==,!=, >, <, >=,<=
+# x = 20
+# y = 4
+# print(x < y)
+# print(x != y)
+
+# ========== Logical Operator ======== and,or,not
+
+# a = 4
+# print(not (a > 3 and a < 10))
+# print(a > 20 or a < 10)
+# print(a > 2 and a < 10)
+#
+# username = "Sneha"
+# password = "admin"
+#
+# if username == "Sneha" and password == 'admin':
+#     print("Welcome to my page")
+# else:
+#     print("Sorry username & password not match")
+
+# ====== Identity == is, is not, === Membership in not in ====
+#
+# name = "Sophia"
+# print('z' not in name)
+
+# a = 3
+# b = 4
+# c = a
+# print(a is c)
+# print(a is not b)
+# print(c is a)
+
+# a = 3
+# print(a is 3)
+
+# ============ Bitwise Operator: &, !, ^, ~, <<, >> ========
+# a = 2  # 00000010
+# b = 3  # 00000011 = 010
+# print(a ^ b)
+
+# p = 10
+# t = 10
+# r = 10
+# print(p * t * r / 100)
+
+# print((2 + 3) * (5 - 3) / 2)
